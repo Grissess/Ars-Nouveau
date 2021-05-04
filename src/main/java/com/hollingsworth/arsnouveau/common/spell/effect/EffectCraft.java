7e81adaaf43a2aaf95941b6d728c795b603e62e3
Grissess <grissess@nexusg.org>
2021-05-04 01:33:53 -0400

    Allow more flexible targeting with craft/ender chests

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectCraft.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectCraft.java
@@ -4,6 +4,7 @@ import com.hollingsworth.arsnouveau.GlyphLib;
 import com.hollingsworth.arsnouveau.api.spell.AbstractAugment;
 import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 import com.hollingsworth.arsnouveau.api.spell.SpellContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -12,6 +13,7 @@ import net.minecraft.inventory.container.WorkbenchContainer;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -29,11 +31,14 @@ public class EffectCraft extends AbstractEffect {
 
     @Override
     public void onResolve(RayTraceResult rayTraceResult, World world, @Nullable LivingEntity shooter, List<AbstractAugment> augments, SpellContext spellContext) {
-        if(shooter instanceof PlayerEntity && isRealPlayer(shooter)){
-            PlayerEntity playerEntity = (PlayerEntity) shooter;
-            playerEntity.openMenu(new SimpleNamedContainerProvider((id, inventory, player) -> {
-                return new CustomWorkbench(id, inventory, IWorldPosCallable.create(player.getCommandSenderWorld(), player.blockPosition()));
-            }, CONTAINER_NAME));
+        if(rayTraceResult instanceof EntityRayTraceResult) {
+            Entity hit = ((EntityRayTraceResult) rayTraceResult).getEntity();
+            if (hit instanceof LivingEntity && isRealPlayer((LivingEntity) hit)) {
+                PlayerEntity playerEntity = (PlayerEntity) hit;
+                playerEntity.openMenu(new SimpleNamedContainerProvider((id, inventory, player) -> {
+                    return new CustomWorkbench(id, inventory, IWorldPosCallable.create(player.getCommandSenderWorld(), player.blockPosition()));
+                }, CONTAINER_NAME));
+            }
         }
     }
 
@@ -65,6 +70,6 @@ public class EffectCraft extends AbstractEffect {
 
     @Override
     public String getBookDescription() {
-        return "Opens the crafting menu.";
+        return "Opens the target's crafting menu.";
     }
 }
--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectEnderChest.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectEnderChest.java
@@ -4,6 +4,7 @@ import com.hollingsworth.arsnouveau.GlyphLib;
 import com.hollingsworth.arsnouveau.api.spell.AbstractAugment;
 import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 import com.hollingsworth.arsnouveau.api.spell.SpellContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EnderChestInventory;
@@ -11,6 +12,7 @@ import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -32,18 +34,21 @@ public class EffectEnderChest extends AbstractEffect {
 
     @Override
     public void onResolve(RayTraceResult rayTraceResult, World world, LivingEntity shooter, List<AbstractAugment> augments, SpellContext spellContext) {
-        if(shooter instanceof PlayerEntity && !(shooter instanceof FakePlayer)){
-            EnderChestInventory chestInventory = ((PlayerEntity)shooter).getEnderChestInventory();
-            ((PlayerEntity) shooter).openMenu(new SimpleNamedContainerProvider((p_226928_1_, p_226928_2_, p_226928_3_) -> {
-                return ChestContainer.threeRows(p_226928_1_, p_226928_2_, chestInventory);
-            }, CONTAINER_NAME));
+        if(rayTraceResult instanceof EntityRayTraceResult) {
+            Entity hit = ((EntityRayTraceResult) rayTraceResult).getEntity();
+            if (hit instanceof LivingEntity && isRealPlayer((LivingEntity) hit)) {
+                EnderChestInventory chestInventory = ((PlayerEntity) hit).getEnderChestInventory();
+                ((PlayerEntity) shooter).openMenu(new SimpleNamedContainerProvider((p_226928_1_, p_226928_2_, p_226928_3_) -> {
+                    return ChestContainer.threeRows(p_226928_1_, p_226928_2_, chestInventory);
+                }, CONTAINER_NAME));
+            }
         }
 
     }
 
     @Override
     public String getBookDescription() {
-        return "Opens your personal ender chest inventory from anywhere.";
+        return "Opens the target's ender chest inventory.";
     }
 
     @Override
